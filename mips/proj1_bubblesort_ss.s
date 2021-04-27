90e3671f684c76950c9741b6d8d46841384f3de7
Rolf Anderson <asaprolfy@gmail.com>
2021-04-26 22:45:19 -0500

    Beginning testing

--- mips/proj1_bubblesort_ss.s
+++ mips/proj1_bubblesort_ss.s
@@ -10,33 +10,64 @@ arr:
         .word	1 #4
 .text
 main:
-  li $sp, 0x10011000
+  li 	$sp, 0x10011000
+  nop
+  nop
+  nop
   sub	$sp, $sp, 32
+  nop
+  nop
   addi	$a0, $sp, 8
   addi	$a1, $zero, 20
+  nop
+  nop
   sw	$a1, 4($sp)
   addi	$t1, $zero, 5
+  nop
+  nop
   sw	$t1, 0($a0)
   addi	$t1, $zero, 4
+  nop
+  nop
   sw	$t1, 4($a0)
   addi	$t1, $zero, 3
+  nop
+  nop
   sw	$t1, 8($a0)
   addi	$t1, $zero, 2
+  nop
+  nop
   sw	$t1, 12($a0)
   addi	$t1, $zero, 1
+  nop
+  nop
   sw	$t1, 16($a0)
   addi	$t1, $zero, 0
 bubble:
+  nop
+  nop
+  nop
+  nop
   addi  $t8, $sp, 8     #ptr:     original pointer to a[]
   addi  $t7, $zero, 0   #bool:    swapped
   addi  $t6, $zero, 4   #int:     i
 
 whileloop:
+  nop
+  nop
+  nop
+  nop
   slt   $t9, $t6, $a1
+  nop
+  nop
   bne   $t9, 1, isswapped
   lw	$t0, 0($t8)             #load a = a[i]
   lw	$t1, 4($t8)             #load b = a[i+1]
+  nop
+  nop
   slt	$t2, $t1, $t0           #if a[i+1] < a[i]
+  nop
+  nop
   bne	$t2, 1, preloop        	#if a[i+1] > a[i], go to else
   sw	$t1, 0($t8)             #a[i]   = a[i+1]
   sw	$t0, 4($t8)             #a[i+1] = a[i]
@@ -44,13 +75,25 @@ whileloop:
   j	preloop                 #go to preloop
 
 preloop:
+  nop
+  nop
+  nop
+  nop
   addi    $t6, $t6, 4           #i++
   addi    $t8, $t8, 4           #a[]++
   j       whileloop             #go back to loop
 isswapped:
+  nop
+  nop
+  nop
+  nop
   beq	$t7, 1, bubble
 
 whiledone:
+  nop
+  nop
+  nop
+  nop
   lw	$t0, 0($a0)
   lw	$t1, 4($a0)
   lw	$t2, 8($a0)
--- mips/proj1_cf_ss.s
+++ mips/proj1_cf_ss.s
@@ -2,17 +2,28 @@
 
 .text
 main:
-	li $sp, 0x10011000
+	li 	$sp, 0x10011000
 	addi	$a0, $zero, 0x0
 	addi	$a1, $zero, 0x1
 	addi	$a2, $zero, 0x2
 	jal	stackframetst
+	nop
+	nop
+	nop
+	nop
 	addi	$s0, $v0, 0
 	j	finish
 
 stackframetst:
+	nop
+	nop
+	nop
+	nop
 	addi	$sp, $sp, -16
 	slti	$t4, $a2, 0xF00
+	nop
+	nop
+	nop
 	beq	$t4, 0, tstdone
 	sw	$ra, 0($sp)
 	sw	$a0, 4($sp)
@@ -22,27 +33,46 @@ stackframetst:
 	j	stcktstloop
 	
 stcktstloop:
+	nop
+	nop
+	nop
+	nop
 	lw	$t0, 4($sp)
 	lw	$t1, 8($sp)
 	lw	$t2, 12($sp)
 	add	$t1, $t1, $t0
+	nop
+	nop
 	add	$t2, $t2, $t1
 	addi	$t0, $t0, 1
+	nop
 	addi	$a2, $t2, 0
 	addi	$a1, $t1, 0
 	addi	$a0, $t0, 0
 	j	stackframetst
 tstdone:
 	addi	$sp, $sp, 16
+	nop
+	nop
+	nop
 	lw	$t0, 4($sp)
 	lw	$t1, 8($sp)
 	lw	$t2, 12($sp)
+	nop
 	add	$t1, $t1, $t0
+	nop
+	nop
 	add	$t2, $t2, $t1
+	nop
+	nop
 	add	$v1, $v1, $t2
 	bne	$t0, 1, tstdone
 	lw	$ra, 0($sp)
 	jr	$ra
 	
 finish:
+	nop
+	nop
+	nop
+	nop
 	halt
