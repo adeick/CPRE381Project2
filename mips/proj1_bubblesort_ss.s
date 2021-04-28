d6f0ecd74b1162146b04431f7b81544af90ca762
Deick <adeick@iastate.edu>
2021-04-28 16:52:23 -0500

    Finished bubblesort and cf SS

--- mips/proj1_bubblesort_ss.s
+++ mips/proj1_bubblesort_ss.s
@@ -10,172 +10,118 @@ arr:
         .word	1 #4
 .text
 main:
-  lui 	$at, 0x00001001
-  nop
-  nop
-  nop
-  nop
-  ori 	$sp, $at, 0x1000
-  nop
-  nop
-  nop
-  nop
+  lui 	$1, 0x00001001
+      nop
+      nop
+      nop
+  ori 	$sp, $1, 0x1000
+      nop
+      nop
+      nop
   addi	$sp, $sp, -32
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   addi	$a0, $sp, 8
-  nop
-  nop
-  nop
   addi	$a1, $zero, 20
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$a1, 4($sp)
-  nop
-  nop
-  nop
-  nop
   addi	$t1, $zero, 5
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$t1, 0($a0)
-  nop
-  nop
   addi	$t1, $zero, 4
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$t1, 4($a0)
-  nop
-  nop
   addi	$t1, $zero, 3
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$t1, 8($a0)
-  nop
-  nop
   addi	$t1, $zero, 2
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$t1, 12($a0)
-  nop
-  nop
-  nop
   addi	$t1, $zero, 1
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   sw	$t1, 16($a0)
-  nop
-  nop
   addi	$t1, $zero, 0
 bubble:
   addi  $t8, $sp, 8     #ptr:     original pointer to a[]
   addi  $t7, $zero, 0   #bool:    swapped
   addi  $t6, $zero, 4   #int:     i
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
 whileloop:
-  slt   $t9, $t6, $a1
-  nop
-  nop
-  nop
-  bne   $t9, 1, isswapped
-  nop
-  nop
-  nop
-  nop
+  slt $t9, $t6, $a1
+      nop
+      nop
+      nop
+  addi $1, $0, 1
+      nop
+      nop
+      nop
+  bne  $t9, $1, isswapped
+      nop
+      nop
+      nop
+      nop
   lw	$t0, 0($t8)             #load a = a[i]
   lw	$t1, 4($t8)             #load b = a[i+1]
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
   slt	$t2, $t1, $t0           #if a[i+1] < a[i]
-  nop
-  nop
-  nop
-  bne	$t2, 1, preloop        	#if a[i+1] > a[i], go to else
-  nop
-  nop
-  nop
-  nop
-  nop
+  addi $1, $0, 1
+      nop
+      nop
+      nop
+  bne	$t2, $1, preloop        	#if a[i+1] > a[i], go to else
+      nop
+      nop
+      nop
+      nop
   sw	$t1, 0($t8)             #a[i]   = a[i+1]
-  nop
-  nop
-  nop
-  nop
   sw	$t0, 4($t8)             #a[i+1] = a[i]
-  nop
-  nop
-  nop
-  nop
   addi	$t7, $zero, 1           #set swapped to true
-  nop
-  nop
-  nop
-  nop
   j	preloop                 #go to preloop
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
+      nop
 
 preloop:
   addi    $t6, $t6, 4           #i++
-  nop
-  nop
-  nop
-  nop
   addi    $t8, $t8, 4           #a[]++
-  nop
-  nop
-  nop
-  nop
   j       whileloop             #go back to loop
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
+      nop
 isswapped:
   beq	$t7, 1, bubble
-  nop
-  nop
-  nop
-  nop
+      nop
+      nop
+      nop
+      nop
 
 whiledone:
   lw	$t0, 0($a0)
-  nop
-  nop
-  nop
-  nop
   lw	$t1, 4($a0)
-  nop
-  nop
-  nop
-  nop
   lw	$t2, 8($a0)
-  nop
-  nop
-  nop
-  nop
   lw	$t3, 12($a0)
-  nop
-  nop
-  nop
   lw	$t4, 16($a0)
-  nop
-  nop
-  nop
-  #halt
+  halt
+      nop
+      nop
+      nop
+      nop
